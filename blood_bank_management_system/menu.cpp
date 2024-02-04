4e387b1b7f61485275dccc442de35e8b262e7cd9
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2024-02-04 23:50:27 +0000

    refactoring code

--- blood_bank_management_system/menu.cpp
+++ blood_bank_management_system/menu.cpp
@@ -10,34 +10,36 @@
 #include "io_utility.hpp"
 #include "data.hpp"
 
-class Menu
-{
-private:
-    std::map<std::string, std::string> data{};
-    Checker myChecker{};
+#include "Menu.hpp"
 
-public:
-    Menu() = default;
-    Menu(const std::vector<std::string> &choice_data) : data(DataMenu()(choice_data))
-    {
-        std::set<std::string> myset{};
-        for (size_t i = 1; i <= choice_data.size(); ++i)
-            myset.insert(std::to_string(i));
-        myChecker.set_data(myset);
-    }
+// class Menu
+// {
+// private:
+//     std::map<std::string, std::string> data{};
+//     Checker myChecker{};
 
-    bool check(const std::string &input)
-    {
-        return myChecker(input);
-    }
+// public:
+//     Menu() = default;
+//     Menu(const std::vector<std::string> &choice_data) : data(DataMenu()(choice_data))
+//     {
+//         std::set<std::string> myset{};
+//         for (size_t i = 1; i <= choice_data.size(); ++i)
+//             myset.insert(std::to_string(i));
+//         myChecker.set_data(myset);
+//     }
 
-    std::string operator()(const std::string &input)
-    {
-        return data[input];
-    }
+//     bool check(const std::string &input)
+//     {
+//         return myChecker(input);
+//     }
 
-    friend std::ostream &operator<<(std::ostream &, const Menu &);
-};
+//     std::string operator()(const std::string &input)
+//     {
+//         return data[input];
+//     }
+
+//     friend std::ostream &operator<<(std::ostream &, const Menu &);
+// };
 
 int main()
 {
